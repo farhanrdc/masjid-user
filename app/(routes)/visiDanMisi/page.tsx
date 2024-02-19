6fdf8490494eb0c0feb436b81e6f9ca4005d46f7
Farhan Ramadhan <farhanramadhan4944@gmail.com>
2024-02-19 09:00:46 +0700

    third commit: Perubahan Pada visi lagi

--- app/(routes)/visiDanMisi/page.tsx
+++ app/(routes)/visiDanMisi/page.tsx
@@ -1,5 +1,5 @@
 import getMisi from '@/actions/get-misi';
-import getVisi from '@/actions/get-visi';
+// import getVisi from '@/actions/get-visi';
 
 import Container from '@/components/ui/container'
 import Visi from '@/components/visi';
@@ -8,13 +8,12 @@ import Misi from '@/components/misi';
 export const revalidate = 0
 
 const ProfilVisiMisi = async () => {
-  const visi = await getVisi("01a575da-bae6-42fe-bfa5-9e505fbd737a")
   const misi = await getMisi()
   return (
    <div className='pb-10'>
     <Container>
         
-          <Visi data={visi}/>
+          <Visi />
           <Misi data={misi}/>
 
     </Container> 
